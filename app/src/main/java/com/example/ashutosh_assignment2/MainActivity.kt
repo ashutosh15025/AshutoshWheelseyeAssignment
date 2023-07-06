1a0fcf1693c6aa7ce97011acb08b2d92f37a5272
ashututosh15025 <ashutosh615singh@gmail.com>
2023-07-06 21:14:36 +0530

    Main activity change

--- app/src/main/java/com/example/ashutosh_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/ashutosh_assignment2/MainActivity.kt
@@ -9,33 +9,11 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
-    lateinit var adapter: NewsAdapter
-    lateinit var recylclerview :RecyclerView
+class MainActivity : Invoker() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       getNews()
-    }
-
-    private fun getNews() {
-        val news: Call<News> = NewsService.newsInstance.getHeadlines("in",1)
-        news.enqueue(object: Callback<News>{
-            override fun onResponse(call: Call<News>, response: Response<News>) {
-
-                val news : News? = response.body()
-                if(news!=null){
-                   adapter=NewsAdapter(this@MainActivity,news.articles)
-                    recylclerview=findViewById(R.id.NewsList);
-                    recylclerview.adapter=adapter;
-                    recylclerview.layoutManager = LinearLayoutManager(this@MainActivity)
-                    Log.d("Done", "onResponse: No Error ")
-                }
-            }
-
-            override fun onFailure(call: Call<News>, t: Throwable) {
-                Log.d("Done", "onResponse:  Error fetching"+t.message.toString())
-            }
-        })
+        getNews()
     }
 }
